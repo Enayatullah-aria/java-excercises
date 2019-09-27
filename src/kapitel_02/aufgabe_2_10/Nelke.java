018f0c0b9c37643a96776256c7ad906440be2d3b
ariae <Enayatullah.aria@msg.group>
2019-09-27 11:21:20 +0200

    kapitel_02 Klasse Nelke addiert

---
+++ src/kapitel_02/aufgabe_2_10/Nelke.java
@@ -0,0 +1,18 @@
+package kapitel_02.aufgabe_2_10;
+
+public class Nelke extends Blumen {
+    // Konstruktordefinition
+    Nelke() {
+        super(2, BLAU);
+    }
+
+    // Die Methode der Oberklasse wird überschrieben und aufgerufen
+    public void identifizieren() {
+        super.identifizieren();
+        System.out.print("Nelke");
+    }
+
+    public void stauden() {
+        System.out.println(" und ist eine Staude");
+    }
+}
